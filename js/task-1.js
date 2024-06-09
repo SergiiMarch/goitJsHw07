1e3869216b4d098528a545c23a938b1a8dffffc3
Sergii Marchenko <marikmarchelo@gmail.com>
2024-06-09 17:02:25 +0300

    add comit

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,17 @@
 //Отримуємо DOM елемент та виводім в консоль кількість LI
 const liEl = document.querySelectorAll("#categories .item");
 console.log("Number of categories:", liEl.length);
-
-//ForEach перебераємо елемнт та виводимо в консоль заголовкі h2 та кількість ul>li
-liEl.forEach((item) => {
+const newArr = Array.from(liEl);
+newArr.map((item) => {
   const title = item.querySelector("h2").textContent;
   const elements = item.querySelectorAll("ul > li").length;
   console.log("Category:", title);
   console.log("Elements:", elements);
 });
+//ForEach перебераємо елемнт та виводимо в консоль заголовкі h2 та кількість ul>li
+// liEl.forEach((item) => {
+//   const title = item.querySelector("h2").textContent;
+//   const elements = item.querySelectorAll("ul > li").length;
+//   console.log("Category:", title);
+//   console.log("Elements:", elements);
+// });
