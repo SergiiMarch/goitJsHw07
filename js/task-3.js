543a756fa344a13ea5e3dc8474e5adc01094734d
Sergii Marchenko <marikmarchelo@gmail.com>
2024-06-11 07:37:07 +0300

    Add task 7/4

--- js/task-3.js
+++ js/task-3.js
@@ -4,5 +4,6 @@ const spanEl = document.getElementById("name-output");
 inputEl.addEventListener("input", handlerInput);
 
 function handlerInput(e) {
-  inputEl.value = (spanEl.textContent = inputEl.value.trim()) || "Anonymous";
+  const trimValue = inputEl.value.trim();
+  spanEl.textContent = trimValue || "Anonimus";
 }
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector(".login-form");
+console.dir(formEl);
 
+formEl.addEventListener("submit", handlerSabmit);
+
+function handlerSabmit(e) {
+  e.preventDefault();
+  const form = e.target;
+  const emailEl = e.target.elements.email.value.trim();
+  const passwordEl = e.target.elements.password.value.trim();
+  if (!emailEl || !passwordEl) {
+    return alert("'All form fields must be filled in'");
+  }
+
+  const formData = {
+    email: emailEl,
+    password: passwordEl,
+  };
+
+  console.log(formData);
+
+  e.target.reset();
+}
