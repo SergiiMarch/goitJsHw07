90a55d5bdeb7cd6c475903179917450833a99c4f
Sergii Marchenko <marikmarchelo@gmail.com>
2024-06-11 13:12:50 +0300

    Add task 7/5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  body: document.querySelector("body"),
+  divEl: document.querySelector(".widget"),
+  spanEl: document.querySelector(".color"),
+  button: document.querySelector(".change-color"),
+};
+
+refs.button.addEventListener("click", hundlerClick);
+
+function hundlerClick(e) {
+  const rendomColor = getRandomHexColor();
+  if (refs.button) {
+    refs.body.style.backgroundColor = rendomColor;
+    refs.spanEl.textContent = rendomColor;
+  }
+}
